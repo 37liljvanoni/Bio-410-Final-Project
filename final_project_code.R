138ccb2adfe8725d4de7d5d7b567ef66e2422d37
37liljvanoni <37liljvanoni@gmail.com>
2026-05-08 14:07:10 -0500

    Add files via upload

--- final_project_code.R
+++ final_project_code.R
@@ -1,3 +1,5 @@
+library(Biostrings)
+library(DECIPHER)
 # Import sequence files
 allcontigs <- c()
 for(i in 1:6){
@@ -6,17 +8,12 @@ allcontigs <- c(allcontigs, contigs)
 }
 # This loop outputs a list, so we flatten the list
 allcontigs <- do.call(c, allcontigs)
-# Align
 toalign <- allcontigs[which(nchar(allcontigs) > 5000)]
 names(toalign) <- 1:length(toalign)
-AlignSeqs(toalign)
-# Visualize alignment
-BrowseSeqs(toalign)
-Treeline(toalign=NULL, method = "ML", showPlot = TRUE)
-Treeline(toalign, method = "ML", showPlot = TRUE)
+# Align
 alignment <- AlignSeqs(toalign)
+# Visualize alignment
 BrowseSeqs(alignment, htmlFile = 'finalproject.html')
 # Produce tree image
-tree <- Treeline(alignmenty, method = "ML", showPlot = TRUE)
 tree <- Treeline(alignment, method = "ML", showPlot = TRUE)
 savehistory("C:/Users/jacqv/Desktop/bioinfo/code for final p.Rhistory")
